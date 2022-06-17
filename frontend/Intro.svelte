4298820aa1c0798ac0c41424b6356688e189e717
fermartz <fernandomvc@gmail.com>
2022-06-17 06:40:40 -0500

    added theme

--- frontend/Intro.svelte
+++ frontend/Intro.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import logo from "./assets/logo-dark.svg"
+  import logo from "./assets/logo.svg"
   import { onMount } from "svelte"
   import { counter } from "canisters/counter"
 
@@ -22,65 +22,57 @@
   <img src={logo} style="height: 15vmin;" alt="logo" />
   
   <p class="text-4xl my-8" >OG Svelte Starter</p>
-  <div class="flex text-left p-8 rounded-xl flex-col bg-slate-100">
-    <div class="my-1">
-      <code>npm run dev:</code>
-      <span> Runs the development server</span>
-    </div>
-    <div class="my-1">
-      <code>npm run build:</code>
-      <span> Builds your frontend for production</span>
-    </div>
-    <div class="my-1">
-      <code>npm run serve:</code>
-      <span> Serves your production-built frontend locally</span>
-    </div>
-    <hr />
-    <div class="my-1">
-      <code>dfx deploy:</code>
-      <span> Compiles & deploys your canisters</span>
-      
-    </div>
-    <div class="text-center text-base mt-8" >
-      <a
-        class="text-indigo-600"
-        href="https://svelte.dev/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Svelte
-      </a>
-      {" | "}
-      <a
-        class="text-indigo-600"
-        href="https://sdk.dfinity.org/docs/developers-guide/sdk-guide.html"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        IC SDK Docs
-      </a>
-      {" | "}
-      <a
-        class="text-indigo-600"
-        href="https://vitejs.dev/guide/features.html"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Vite Docs
-      </a>
-      
-      {" | "}
-      <a
-        class="text-indigo-600"
-        href="https://tailwindcss.com/docs/installation"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Tailwind
-      </a>
+
+  <div class="card bg-neutral text-neutral-content">
+    <div class="card-body items-center text-center">
+      <!-- <h2 class="card-title">Cookies!</h2> -->
+      <p>npm run dev : Runs the development server </p>
+      <p>npm run build : Builds your frontend for production </p>
+      <p>npm run serve : Serves your production-built frontend locally </p>
+      <div class="card-actions justify-end">
+        <div class="text-center text-base mt-8" >
+          <a
+            class="text-indigo-600"
+            href="https://svelte.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Svelte
+          </a>
+          {" | "}
+          <a
+            class="text-indigo-600"
+            href="https://sdk.dfinity.org/docs/developers-guide/sdk-guide.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            IC SDK Docs
+          </a>
+          {" | "}
+          <a
+            class="text-indigo-600"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+          
+          {" | "}
+          <a
+            class="text-indigo-600"
+            href="https://tailwindcss.com/docs/installation"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tailwind
+          </a>
+        </div>
+      </div>
     </div>
   </div>
-  <button type="button" class="w-48 flex items-center justify-center my-6 px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10" on:click={increment}>
+ 
+  <button type="button" class="btn btn-primary my-8" on:click={increment}>
     Count is: {count}
   </button>
   <p style="text-sm">
